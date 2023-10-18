bf91d157c73f96dea407054e1c7ea52e41c11970
Rahil Kothari <kotharirahil9@gmail.com>
2023-10-18 22:58:53 +0530

    Add search function(untested)

--- studyplanner.c
+++ studyplanner.c
@@ -12,8 +12,22 @@ typedef struct node {
 
 void displaynode(node * n){
      printf("The subject name is: %s \n The number of hours is: %d \n The marks alotted are %d", n->name, n->hours, n->marks);
+     
 }
 
+
+void search(node * root, char s[MAX_SIZE], node ** found) {
+
+    if(strcmp(root->name, s))
+    {
+        *found = root;
+        return;
+    }
+    for(int i = 0; i < root->children; i++)
+    {
+        search(root->arr[i], s, found);
+    }
+}
 
 int main(){
     node *head = (node *)malloc(sizeof(node));
